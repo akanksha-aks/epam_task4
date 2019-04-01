d879e9689f3a4009bd7e7f633b4ae89f79391965
akanksha-aks <asingh98f@gmail.com>
2019-04-01 21:36:58 +0530

    node structure

---
+++ Node.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author akanksha singh
+ */
+public class Node {
+	
+    Node next;
+    String data;
+    
+    public Node(String data) {
+	super();
+	this.data = data;
+    }
+}
